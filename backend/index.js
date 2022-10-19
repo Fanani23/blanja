e0f79329f58b6a148adcb2ba7150ae17546ababb
Fanani23 <pramudiasf23@gmail.com>
2022-10-19 12:36:34 +0700

    update index, insert category & transaction

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
+import CategoryRoute from "./routes/CategoryRoute.js";
+import TransactionRoute from "./routes/TransactionRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(ProductRoute);
+app.use(CategoryRoute);
+app.use(TransactionRoute);
 
 app.listen(3023, ()=> console.log('Server up and running...'));
