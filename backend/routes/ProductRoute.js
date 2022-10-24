5ab5aedaa2a5765aa6e45d94261b15f6b9279dfb
Fanani23 <pramudiasf23@gmail.com>
2022-10-25 05:00:10 +0700

    update product route check create and update route

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -6,13 +6,17 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/ProductController.js";
+import {
+  checkCreateProduct,
+  checkUpdateProduct,
+} from "../middleware/CheckProduct.js";
 
 const router = express.Router();
 
 router.get("/product", getProduct);
 router.get("/product/:id", getProductById);
-router.post("/product", createProduct);
-router.put("/product/:id", updateProduct);
+router.post("/product", checkCreateProduct, createProduct);
+router.put("/product/:id", checkUpdateProduct, updateProduct);
 router.delete("/product/:id", deleteProduct);
 
 export default router;
