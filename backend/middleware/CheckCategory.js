2836fae724d553b88b6f076644645d56c740f728
Fanani23 <pramudiasf23@gmail.com>
2022-10-25 04:06:38 +0700

    add middleware check category

---
+++ backend/middleware/CheckCategory.js
@@ -0,0 +1,25 @@
+export const checkCreateCategory = (req, res, next) => {
+  const { category_name } = req.body;
+  try {
+    if (category_name === "") {
+      throw new Error("Category name can't be empty!");
+    }
+  } catch (err) {
+    return res.send(`${err}`);
+  }
+  next();
+};
+
+export const checkUpdateCategory = (req, res, next) => {
+  const { category_name } = req.body;
+  try {
+    if (category_name === category_name) {
+      throw new Error(
+        "New category name can't be same with old category name!"
+      );
+    }
+  } catch (err) {
+    return res.send(`${err}`);
+  }
+  next();
+};
