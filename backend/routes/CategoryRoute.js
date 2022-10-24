4b8f039ed7cefed3c80a99b802cac3f970b894e9
Fanani23 <pramudiasf23@gmail.com>
2022-10-25 04:07:31 +0700

    update err handling create and update category

--- backend/routes/CategoryRoute.js
+++ backend/routes/CategoryRoute.js
@@ -6,13 +6,17 @@ import {
   updateCategory,
   deleteCategory,
 } from "../controllers/CategoryController.js";
+import {
+  checkCreateCategory,
+  checkUpdateCategory,
+} from "../middleware/CheckCategory.js";
 
 const router = express.Router();
 
 router.get("/category", getCategory);
 router.get("/category/:id", getCategoryById);
-router.post("/category", createCategory);
-router.put("/category/:id", updateCategory);
+router.post("/category", checkCreateCategory, createCategory);
+router.put("/category/:id", checkUpdateCategory, updateCategory);
 router.delete("/category/:id", deleteCategory);
 
 export default router;
